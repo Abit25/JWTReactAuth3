e1dace150a104484fab1ad5071023072292c4b3d
Abit25 <abheet25@gmail.com>
2019-08-23 22:10:14 +0530

    First commit

---
+++ ReactAuth/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ReactauthConfig(AppConfig):
+    name = 'ReactAuth'
---
+++ myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/priya/Desktop/JWT/myenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
